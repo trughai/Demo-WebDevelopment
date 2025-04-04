b12822f0f27c8ad46fb57632238e223b30b1a203
trughai <nguyenhotrunghai54205@gmail.com>
2025-04-05 00:02:05 +0700

    update

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,8 @@
-// Import các module cần thiết từ Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-storage.js";
 
-// Cấu hình Firebase của bạn
 const firebaseConfig = {
   apiKey : "AIzaSyBUBbt3KY14oGSYGD45b4C2pZK0OIdfpyY" , 
   authDomain : "fir-webdevelopment.firebaseapp.com" , 
@@ -15,10 +13,9 @@ const firebaseConfig = {
   measurementId : "G-JEEC5B6SS3"
 };
 
-// Khởi tạo Firebase App
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-// Khởi tạo các service Firestore, Auth và Storage
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export { auth, db, storage };
